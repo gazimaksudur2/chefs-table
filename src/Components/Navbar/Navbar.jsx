865092cb3c2dd50663484a4c014f753fb8ae7f97
gazimaksudur2 <gazimaksudur2@gmail.com>
2024-03-17 00:35:50 +0600

    favicon added properly

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,17 +4,20 @@ const Navbar = () => {
             <div className="navbar bg-base-100 flex justify-between items-center">
                 <div className="navbar-start md:w-1/4">
                     <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm text-[#150B2Bab] dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><a>Home</a></li>
+                            <li><a>Recipes</a></li>
+                            <li><a>Allocation</a></li>
+                            <li><a>About</a></li>
+                        </ul>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm text-[#150B2Bab] dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Home</a></li>
-                        <li><a>Recipes</a></li>
-                        <li><a>Allocation</a></li>
-                        <li><a>About</a></li>
-                    </ul>
+                    <div className="logo-brand flex justify-between items-center gap-3">
+                        <img className="w-10 h-10" src="https://i.ibb.co/wNwbkbh/restaurant.png" alt="official-icon" />
+                        <a className="btn-ghost font-bold text-3xl text-[#150B2B] ">Recipe Calories</a>
                     </div>
-                    <a className="btn-ghost font-bold text-3xl text-[#150B2B] ">Recipe Calories</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal text-[#150B2Bab] px-1 gap-10 text-xl">
