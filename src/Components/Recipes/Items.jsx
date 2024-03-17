a275b70f21884065010a1b59a365fa160f999dd1
gazimaksudur2 <gazimaksudur2@gmail.com>
2024-03-17 06:45:49 +0600

    may be last update

--- src/Components/Recipes/Items.jsx
+++ src/Components/Recipes/Items.jsx
@@ -5,7 +5,7 @@ import PropsType from 'prop-types';
 // eslint-disable-next-line react/prop-types
 const Items = ({handleToCook, scrollToSection}) => {
     const [items, setItems] = useState([]);
-    const [itemCountInfo, setItemCountInfo] = useState('Show Less Items');
+    const [itemCountInfo, setItemCountInfo] = useState('Show All Items');
 
     const setItemsInfo = () => {
         setItemCountInfo((itemCountInfo==='Show All Items')?'Show Less Items':'Show All Items');
@@ -16,7 +16,7 @@ const Items = ({handleToCook, scrollToSection}) => {
         fetch('Recipes.json')
         .then(res => res.json())
         .then(data => {
-            data = (itemCountInfo==='Show Less Items'? data : data.slice(0,4));
+            data = (itemCountInfo==='Show Less Items'? data : data.slice(0,2));
             setItems(data);
         });
     },[itemCountInfo]);
